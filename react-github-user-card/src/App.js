c84cc1cdd2251ec1f89f3864ea368e316c5bf056
osogrizz <lhtor@gmail.com>
2019-11-02 19:36:02 -0700

    Fetches user data and renders data.

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserCard from './components/UserCard'
+
+class App extends Component {
+  constructor(){
+    console.log('Contructor rendered!')
+    super()
+
+    this.state= {
+      user: ''
+    }
+  }
+
+  componentDidMount() {
+    console.log('cDM rendered!')
+    const getUsers =  () => {
+        fetch('https://api.github.com/users/osogrizz')
+      // .then(  res => console.log(res.json()))
+      .then( res => res.json())
+      .then( user => {
+        this.setState({
+          user: user
+        })
+      })
+      .catch(err => console.log(err))
+    }
+    getUsers()
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <UserCard user={this.state.user} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-github-user-card/src/components/UserCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UserCard = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h2>{props.user.name}</h2>
+      <h3>{props.user.login}</h3>
+      <p>Followers: {props.user.followers}</p>
+      
+    </div>
+  )
+}
+
+export default UserCard
